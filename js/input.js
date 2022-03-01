07241ff0a2a32d5c97daa79914b4d1ac631e2aaf
Guilherme Rosário Alves <guilhermerosariosalgado@gmail.com>
2022-03-01 19:11:40 -0300

    Update input.js

--- js/input.js
+++ js/input.js
@@ -1,10 +1,23 @@
-let inputs = document.getElementsByClassName('input-form');
-		for (let input of inputs) { //percorrer inputs
-			input.addEventListener("blur", function() {
-				if(input.value.trim() != ""){
-					input.classList.add("has-val");
-				} else {
-					input.classList.remove("has-val");
-				}
-			});
+var inputs = document.getElementsByClassName('input-form');
+for (let input of inputs) { //percorrer inputs
+	input.addEventListener("blur", function () {
+		if (input.value.trim() != "") {
+			input.classList.add("has-val");
+		} else {
+			input.classList.remove("has-val");
 		}
+	});
+}
+
+let form = document.getElementById('login-form');
+form.addEventListener("submit", function (event) {
+	for (let input of inputs) {
+		if (input.value.trim() != "") {
+			input.parentElement.classList.add("wrap-input-invalid");
+		} else {
+			input.parentElement.classList.remove("wrap-input-invalid");
+		}
+	}
+
+	event.preventDefault(); //Evento finalizado, sem que a pagina seja redirecionada 
+});
