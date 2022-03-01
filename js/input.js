dc08d91811717f91fa215676a02143616df558f0
Guilherme Rosário Alves <guilhermerosariosalgado@gmail.com>
2022-03-01 19:03:34 -0300

    Update input.js

--- js/input.js
+++ js/input.js
@@ -1,5 +1,5 @@
 let inputs = document.getElementsByClassName('input-form');
-		for (let input of inputs) {
+		for (let input of inputs) { //percorrer inputs
 			input.addEventListener("blur", function() {
 				if(input.value.trim() != ""){
 					input.classList.add("has-val");
