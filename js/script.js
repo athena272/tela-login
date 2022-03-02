f9d4ff9fa7191c21836fb70f8ad64ad6d0044e79
Guilherme Rosário Alves <guilhermerosariosalgado@gmail.com>
2022-03-02 14:14:26 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,39 @@
+var inputs = document.getElementsByClassName('input-form');
+for (let input of inputs) { //percorrer inputs
+	input.addEventListener("blur", function () {
+		if (input.value.trim() != "") {
+			input.classList.add("has-val");
+		} else {
+			input.classList.remove("has-val");
+		}
+	});
+}
+
+let form = document.getElementById('login-form');
+form.addEventListener("submit", function (event) {
+	for (let input of inputs) {
+		if (input.value.trim() == "") {
+			input.parentElement.classList.add("wrap-input-invalid");
+		} else {
+			input.parentElement.classList.remove("wrap-input-invalid");
+		}
+	}
+
+	event.preventDefault(); //Evento finalizado, sem que a pagina seja redirecionada 
+});
+
 // Request para Login
 async function requestLogin() {
 	
 	let url = "https://cloud.softgreen.com.br/challenge/login_stage.jsp?token=0c4fb5956d00091319b39929e084b02e0056bf93&key=96de5543d183d7de52ac5fa21c46fc811f673f89"
 	
-	return fetch(url).then(res => res.json()).then(output => {
-		
-		let response = JSON.stringify(output)
+	const response = await fetch(url);
+	
+	let data = await response.json();
 
-		response = JSON.parse(response)[0]
-		
-		return response;
-	})
+	data = JSON.stringify(data);
+
+	return JSON.parse(data)[0];
 	
 };
 
@@ -25,9 +48,9 @@ function openModal() {
 	let response = requestLogin();
 
 	// response.then((x) => console.log(x.result));
-	response.then((x) => {
+	response.then((message) => {
 
-		document.getElementById('status').innerHTML = "Result: " + x.result
+		document.getElementById('status').innerHTML = "Result: " + message.result
 	
 	});
 
