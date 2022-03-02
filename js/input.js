e8e65fdb33669752be377182e84629ad5dcfa85a
Guilherme Rosário Alves <guilhermerosariosalgado@gmail.com>
2022-03-02 12:14:22 -0300

    Update input.js

--- js/input.js
+++ js/input.js
@@ -22,3 +22,21 @@ form.addEventListener("submit", function (event) {
 	event.preventDefault(); //Evento finalizado, sem que a pagina seja redirecionada 
 });
 
+// Request para Login
+
+function requestLogin() {
+	// let url = "https://cloud.softgreen.com.br/challenge/login_stage.jsp?token=23e7d522c465018a590d456233a8f114a1207210&key=ba94792bcddbec068cfc784634cd67ea635723a7"
+
+	let url = "https://cloud.softgreen.com.br/challenge/login_stage.jsp?token=ukraine&key=yellow"
+	
+	fetch(url).then(res => res.json()).then(output => {
+		
+		let response = JSON.stringify(output)
+
+		response = JSON.parse(response)[0]
+
+	})
+	
+	return response
+
+};
